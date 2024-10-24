98de10c04b2e6149da21ca164dafd78018ec8828
Jiwon Kim <jiwon05@Andys-MacBook-Air-4.local>
2024-10-24 10:27:44 -0500

    delete nav

--- deatz/src/app/page.tsx
+++ deatz/src/app/page.tsx
@@ -1,20 +1,11 @@
 // src/MainPage.js
 import React from 'react';
+import Navbar from '../components/navbar'; 
 
 const MainPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <header className="bg-gray-800 text-white py-4">
-        <div className="container mx-auto flex items-center justify-between">
-          <h1 className="text-4xl font-bold">DEATZ</h1>
-          <nav className="mt-4 flex space-x-4">
-            <a href="/" className="text-lg">Home</a>
-            <a href="/about" className="text-lg">About</a>
-            <a href="/contact" className="text-lg">Contact</a>
-            <a href="/deals" className="text-lg">Deals</a>
-          </nav>
-        </div>
-      </header>
+      <Navbar />
 
       <main className="flex-grow">
         <section className="relative pb-32">
--- deatz/src/components/navbar.tsx
+++ deatz/src/components/navbar.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-function navbar() {
-    return {
-        
-    }
-}
+
+const Navbar = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <h1 className="text-4xl font-bold">DEATZ</h1>
+        <nav className="mt-4 flex space-x-4">
+          <a href="/" className="text-lg">Home</a>
+          <a href="/about" className="text-lg">About</a>
+          <a href="/contact" className="text-lg">Contact</a>
+          <a href="/deals" className="text-lg">Deals</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
