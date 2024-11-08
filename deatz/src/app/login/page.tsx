c62a80cf5e451d90210c20134f42dda621fb4c5a
jwang0521 <145153632+jwang0521@users.noreply.github.com>
2024-11-08 16:50:27 -0600

    Add new feature

---
+++ deatz/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+const Login = () => {
+  return (
+    <div className="login container">
+      <h1>Thi is login page</h1>
+    </div>
+  );
+};
+
+export default Login;
